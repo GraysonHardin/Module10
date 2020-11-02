842b3a6e9b7b5fe6745c23c9950359ac761e7740
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-11-02 10:05:13 -0600

    Added docstring and comments

--- unit_test_assignment/student_class.py
+++ unit_test_assignment/student_class.py
@@ -1,3 +1,12 @@
+"""
+Program: student_class.py
+Author: Grayson Hardin
+Last date modified: 11/2/2020
+
+This program takes a first and last name, major and gpa. It will simply print said values to the console. However, it has extensive testing behind to test whether or not the names are correct, if the gpa is a valid input, and more.
+"""
+
+
 class Student:
     def __init__(self, lname, fname, major, gpa=0.0):
         self.last_name = lname
@@ -5,15 +14,16 @@ class Student:
         self.major = major
         self.gpa = gpa
 
+        # Majority of the logic is from 18-20. This will process and evaluate whether the said variables are correct. If so, then they will continue on and print. If not, they will return the error.
         name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
         if not isinstance(gpa, float) or gpa < 0 or gpa > 4 or not (name_characters.issuperset(fname) and name_characters.issuperset(lname) and name_characters.issuperset(major)):
             raise ValueError
 
-    def __str__(self):
+    def __str__(self):  # This is the formatting code
         return self.last_name + ", " + self.first_name + " has major " + self.major + " with gpa: " + str(self.gpa)
 
 
-def main():
+def main():  # Outputs to the console
     grayson_student = Student(lname='Hardin', fname='Grayson', major='CIS', gpa=4.0)
     print(grayson_student)
 
--- unit_test_assignment/test_student_class.py
+++ unit_test_assignment/test_student_class.py
@@ -3,13 +3,13 @@ from unit_test_assignment.student_class import Student
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self):
+    def setUp(self):  # Added setup
         self.student = Student(lname='Hardin', fname='Grayson', major='CIS', gpa=4.0)
 
     def tearDown(self):
         del self.student
 
-    def test_object_created_required_attributes(self):
+    def test_object_created_required_attributes(self): # This sets the expected for the required variables
         grayson_student = Student(lname='Hardin', fname='Grayson', major='CIS')
         expected_last_name = 'Hardin'
         expected_first_name = 'Grayson'
@@ -19,7 +19,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(grayson_student.first_name, expected_first_name)
         self.assertEqual(grayson_student.major, expected_major)
 
-    def test_object_created_all_attributes(self):
+    def test_object_created_all_attributes(self):  # This sets the expected output for all attributes
         grayson_student = Student(lname='Hardin', fname='Grayson', major='CIS', gpa=4.0)
         expected_last_name = 'Hardin'
         expected_first_name = 'Grayson'
@@ -31,31 +31,31 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(grayson_student.major, expected_major)
         self.assertEqual(grayson_student.gpa, expected_gpa)
 
-    def test_student_str(self):
+    def test_student_str(self):  # Tests whether the student value is str
         expected = f'Hardin, Grayson has major CIS with gpa: 4.0'
         self.assertEqual(self.student.__str__(), expected)
 
-    def test_object_not_created_error_last_name(self):
+    def test_object_not_created_error_last_name(self):  # Checks whether the last name is correct
         with self.assertRaises(ValueError):
             Student(lname='123', fname='Grayson', major='CIS', gpa=4.0)
 
-    def test_object_not_created_error_first_name(self):
+    def test_object_not_created_error_first_name(self):  # Checks whether the first name is correct
         with self.assertRaises(ValueError):
             Student(lname='Hardin', fname='123', major='CIS', gpa=4.0)
 
-    def test_object_not_created_error_major(self):
+    def test_object_not_created_error_major(self):  # Checks to see if value error was raised given bad major
         with self.assertRaises(ValueError):
             Student(lname='Hardin', fname='Grayson', major='123', gpa=4.0)
 
-    def test_object_not_created_error_gpa(self):
+    def test_object_not_created_error_gpa(self):  # Checks to see if value error was raised given bad gpa
         with self.assertRaises(ValueError):
             Student(lname='Hardin', fname='Grayson', major='CIS', gpa='invalid value')
 
-    def test_object_gpa_range_is_below_range(self):
+    def test_object_gpa_range_is_below_range(self):  # Checks to see if value error was raised given negative gpa
         with self.assertRaises(ValueError):
             Student(lname='Hardin', fname='Grayson', major='CIS', gpa=-0.01)
 
-    def test_object_gpa_range_is_above_range(self):
+    def test_object_gpa_range_is_above_range(self):  # Checks whether the value error was raised given a gpa is too high
         with self.assertRaises(ValueError):
             Student(lname='Hardin', fname='Grayson', major='CIS', gpa=4.01)
 
